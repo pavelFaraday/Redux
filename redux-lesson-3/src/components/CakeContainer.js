da1ae87785523c6e17eaea8d4c89b1113370244c
pavelFaraday <devstudio.corp@gmail.com>
2021-08-28 10:34:11 +0400

    Start redux-lesson-3

---
+++ redux-lesson-3/src/components/CakeContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function CakeContainer() {
+	return (
+		<div>
+			<h3>Number of Cakes</h3>
+			<button>Buy Cake</button>
+		</div>
+	);
+}
+
+export default CakeContainer;
